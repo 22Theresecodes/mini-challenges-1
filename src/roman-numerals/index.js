52aa676ff4ef9b46284774ae5a7548e058226001
22Theresecodes <105355739+22Theresecodes@users.noreply.github.com>
2022-09-12 14:44:45 +0100

    work done

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,56 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+
+const romanVariable = {
+    I:1,
+    V:5,
+    X:10,
+    L:50,
+    C:100,
+    D:500,
+    M:1000
+}
+
+
+
+// finds decimal value of a given roman numeral
+function romanToDecimal(str) 
+{
+    // initialize result
+    let result = 0;
+    // Loop through elements of str
+    for (i=0; i<str.length; i++)
+    {
+        if (str[i] == 'I' && str[i+1] == 'V') {
+            result += 4
+            i++
+        } else if (str[i]=='I' && str[i+1] =='X') {
+            result += 9
+            i++
+        } else if (str[i]== 'X' && str[i+1] == 'L') {
+            result += 40
+            i++
+        } else if (str[i]== 'X' && str[i+1] == 'C') {
+            result += 90
+            i++
+        } else if (str[i]== 'C' && str[i+1] == 'D') {
+            result += 400
+            i++
+        } else if (str[i] == 'C' && str[i+1] == 'M'){
+            result += 900
+            i++
+        } else {
+            result += romanVariable[str[i]]
+        }
+
+    }
+    return result
+}
+console.log(romanToDecimal('XLV'))
+
+
+
+
 
 module.exports = romanToDecimal;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,26 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+  /*   let newArray= []
+    for(let i=0; i>array.length; i++) {
+        newArray.push([]);
+    }
+    for(let i=0; i<array.length; i++){
+    for (let j=0; j<array.length; j++){
+        newArray[j].push(array[i][j]);
+    }
+    }
+    return newArray; */
+
+    let [row] = array
+
+    return row.map((value, column) => array.map(row => row[column]))
+}
+
+console.log(transpose([
+    [1, 7, 9],
+    [2, 3, 5],
+]))
 
 module.exports = transpose;
